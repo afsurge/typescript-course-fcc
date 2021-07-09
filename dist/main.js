21bef34b5bff0c3b215b012ab68b3e5f9b085605
Abrar Faisal <abrarfaisal20@gmail.com>
2021-07-09 22:40:05 +0200

    Interfaces and naming against classes

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,25 @@
 //// variables ////
 var hello = "world";
 // hello = true; --> Error: Type 'true' is not assignable to type 'string'
+//
 //// functions ////
 var getFullName = function (name, surname) {
     return name + " " + surname;
     // return true; --> Error: Type 'true' is not assignable to type 'string'
 };
 console.log(getFullName("Maisha", "Fatema"));
+var user = {
+    name: "Abrar",
+    age: 36,
+    getMessage: function () {
+        return "Hello " + this.name;
+    }
+};
+var user2 = {
+    name: "Anika",
+    getMessage: function () {
+        return "Hello " + this.name;
+    }
+};
+console.log(user.name);
+console.log(user.getMessage());
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 //// variables ////
 let hello: string = "world";
 // hello = true; --> Error: Type 'true' is not assignable to type 'string'
-
+//
 //// functions ////
 const getFullName = (name: string, surname: string): string => {
     return name + " " + surname;
@@ -9,3 +9,31 @@ const getFullName = (name: string, surname: string): string => {
 };
 
 console.log(getFullName("Maisha", "Fatema"));
+//
+//// interfaces ////
+// Interfaces in TS help to describe entities
+// By default all properties inside interface mandatory
+// Postfix for all interfaces to avoid confusion with class names e.g. 'interface IUser' and 'class User'
+interface IUser {
+    name: string;
+    age?: number;
+    getMessage(): string;
+}
+
+const user: IUser = {
+    name: "Abrar",
+    age: 36,
+    getMessage() {
+        return "Hello " + this.name;
+    },
+};
+
+const user2: IUser = {
+    name: "Anika",
+    getMessage() {
+        return "Hello " + this.name;
+    },
+};
+
+console.log(user.name);
+console.log(user.getMessage());
