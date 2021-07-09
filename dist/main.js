17b29fcf55885ac5593bee75c05e45fa29a3ee63
Abrar Faisal <abrarfaisal20@gmail.com>
2021-07-09 22:17:47 +0200

    Variables and Functions

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,9 @@
-var a = "1";
-console.log("aaa", a);
+//// variables ////
+var hello = "world";
+// hello = true; --> Error: Type 'true' is not assignable to type 'string'
+//// functions ////
+var getFullName = function (name, surname) {
+    return name + " " + surname;
+    // return true; --> Error: Type 'true' is not assignable to type 'string'
+};
+console.log(getFullName("Maisha", "Fatema"));
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,11 @@
-const a = "1";
-console.log("aaa", a);
+//// variables ////
+let hello: string = "world";
+// hello = true; --> Error: Type 'true' is not assignable to type 'string'
+
+//// functions ////
+const getFullName = (name: string, surname: string): string => {
+    return name + " " + surname;
+    // return true; --> Error: Type 'true' is not assignable to type 'string'
+};
+
+console.log(getFullName("Maisha", "Fatema"));
