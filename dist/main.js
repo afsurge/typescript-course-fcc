49204fff2cff44b8da5d6f721d93c036f748964e
Abrar Faisal <abrarfaisal20@gmail.com>
2021-07-10 23:13:51 +0200

    Minor udpate

--- dist/main.js
+++ dist/main.js
@@ -1,18 +1,37 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
+var __extends =
+    (this && this.__extends) ||
+    (function () {
+        var extendStatics = function (d, b) {
+            extendStatics =
+                Object.setPrototypeOf ||
+                ({ __proto__: [] } instanceof Array &&
+                    function (d, b) {
+                        d.__proto__ = b;
+                    }) ||
+                function (d, b) {
+                    for (var p in b)
+                        if (Object.prototype.hasOwnProperty.call(b, p))
+                            d[p] = b[p];
+                };
+            return extendStatics(d, b);
+        };
+        return function (d, b) {
+            if (typeof b !== "function" && b !== null)
+                throw new TypeError(
+                    "Class extends value " +
+                        String(b) +
+                        " is not a constructor or null"
+                );
+            extendStatics(d, b);
+            function __() {
+                this.constructor = d;
+            }
+            d.prototype =
+                b === null
+                    ? Object.create(b)
+                    : ((__.prototype = b.prototype), new __());
+        };
+    })();
 //// Working with DOM ////
 //
 // TS has lots of types for DOM out of the box
@@ -44,11 +63,11 @@ var User = /** @class */ (function () {
     };
     User.maxAge = 50;
     return User;
-}());
+})();
 var Admin = /** @class */ (function (_super) {
     __extends(Admin, _super);
     function Admin() {
-        return _super !== null && _super.apply(this, arguments) || this;
+        return (_super !== null && _super.apply(this, arguments)) || this;
     }
     Admin.prototype.setEditor = function (editor) {
         this.editor = editor;
@@ -57,7 +76,7 @@ var Admin = /** @class */ (function (_super) {
         return this.editor;
     };
     return Admin;
-}(User));
+})(User);
 var user = new User("Abrar", "Faisal");
 console.log(user.getFullName());
 // console.log(user.firstName); // Error! firstName is private
